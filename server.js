7194355f7b1ba150be6b5dab301cfc26ed280e3d
alex <7253alex@gmail.com>
2022-04-28 23:14:39 -0700

    edit files for deployment

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: ".env" });
 require("./config/db.js");
 const express = require("express");
 const userRoutes = require("./routes/user.routes.js");
