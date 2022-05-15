9deab05aaa44460d558a0269ebf4d161f92ab9e6
alex <7253alex@gmail.com>
2022-05-15 11:27:30 -0700

    removed duplicate multer code

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,46 +1,7 @@
 require("dotenv").config({ path: "./config/.env" });
 const router = require("express").Router();
 const postController = require("../controllers/post.controller");
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-
-aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  region: process.env.AWS_BUCKET_REGION,
-});
-
-const bucketName = process.env.AWS_BUCKET_NAME;
-const s3 = new aws.S3();
-
-const fileFilter = (req, file, callback) => {
-  if (
-    file.mimetype !== "image/jpg" &&
-    file.mimetype !== "image/png" &&
-    file.mimetype !== "image/jpeg" &&
-    file.mimetype !== "image/webp"
-  ) {
-    return callback(
-      null,
-      false,
-      (req.errors = uploadErrors(new Error("Invalid file format")))
-    );
-  }
-  callback(null, true);
-};
-
-var upload = multer({
-  storage: multerS3({
-    bucket: bucketName,
-    s3: s3,
-    acl: "public-read",
-    key: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
-  fileFilter: fileFilter,
-});
+const { upload } = require("../utils/upload.utils.js")
 
 router.get("/", postController.allPosts); // all posts from database
 router.get("/user/:userId/limit?", postController.getPostsFromUser);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,45 +4,7 @@ const authController = require("../controllers/auth.controller.js");
 const userController = require("../controllers/user.controller.js");
 const uploadController = require("../controllers/upload.controller.js");
 const { uploadErrors } = require("../utils/errors.utils");
-const aws = require("aws-sdk");
-const multer = require("multer");
-const multerS3 = require("multer-s3");
-
-aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  region: process.env.AWS_BUCKET_REGION,
-});
-
-const bucketName = process.env.AWS_BUCKET_NAME;
-const s3 = new aws.S3();
-
-const fileFilter = (req, file, callback) => {
-  if (
-    file.mimetype !== "image/jpg" &&
-    file.mimetype !== "image/png" &&
-    file.mimetype !== "image/jpeg"
-  ) {
-    return callback(
-      null,
-      false,
-      (req.errors = uploadErrors(new Error("Invalid file format")))
-    );
-  }
-  callback(null, true);
-};
-
-var upload = multer({
-  storage: multerS3({
-    bucket: bucketName,
-    s3: s3,
-    acl: "public-read",
-    key: function (req, file, cb) {
-      cb(null, file.originalname)
-    },
-  }),
-  fileFilter: fileFilter,
-});
+const { upload } = require("../utils/upload.utils.js")
 
 // auth
 router.post("/register", authController.signUp);
