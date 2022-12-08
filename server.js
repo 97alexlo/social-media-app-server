cf49551b2c2099938ed88f5292738daaa1d6b271
alex <7253alex@gmail.com>
2022-12-07 19:24:38 -0800

    test cors fix

--- server.js
+++ server.js
@@ -22,11 +22,7 @@ app.set("trust proxy", 1);
 //   preflightContinue: false,
 // };
 // app.use(cors(corsOptions));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.options('*', cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
